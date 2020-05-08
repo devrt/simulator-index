0e9fc3c21d518f7c551111e78dd35159b5719aa9
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2020-05-09 08:08:52 +0900

    add friendly message

--- select.sh
+++ select.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-IMAGE=`yq r /index.yaml 'indexes.*.image' | fzf --preview "yq r /index.yaml 'indexes.(image=={}).description'"`
+IMAGE=`yq r /index.yaml 'indexes.*.image' | fzf --preview "yq r /index.yaml 'indexes.(image=={})'"`
+echo "Updating docker-compose.yml to use ${IMAGE} simulation container..."
 yq w -i /work/docker-compose.yml services.simulator.image $IMAGE
+echo "done."
+echo "Your simulation environment is ready."
+echo "Please enter 'docker-compose up' to launch your environment."
